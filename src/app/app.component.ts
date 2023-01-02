d4e68d1f2aca297cc0f89f4cf4d2c3e7e67b12e9
JohannaChiabotto <106194977+JohannaChiabotto@users.noreply.github.com>
2023-01-02 14:34:06 +0100

    Add addTodo function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,18 @@ export class AppComponent {
     {todo: "fischen", done: true}
   ];
 
+  newTodo= "";
+
+  setTodo(event: KeyboardEvent){
+    this.newTodo = (event.target as HTMLInputElement).value
+  }
+
+  addTodo(){
+    if (this.newTodo.trim() !== ""){
+      this.todos.push({todo: this.newTodo, done: false})
+    }
+  }
+
   toggleTodo(index: number){
     this.todos[index].done = !this.todos[index].done;
   }
